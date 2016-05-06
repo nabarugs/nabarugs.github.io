331f72b5de8cf3468996e004b84461d9ee951d3b
ubu12 <ubu12@ubu12.(none)>
2016-05-06 00:18:02 -0400

    fixed hero height issue

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -142,8 +142,3 @@ $(document).ready(function() {
 	});	
 
 });
-
-   $(document).delegate('*[data-toggle="lightbox"]', 'click', function(event) {
-    event.preventDefault();
-    $(this).ekkoLightbox();
-}); 
