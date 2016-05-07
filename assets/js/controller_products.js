68605f3b40f5c586e79a440a8fae4c038c0aaf9c
ubu12 <ubu12@ubu12.(none)>
2016-05-07 15:53:20 -0400

    on finish render directive

--- assets/js/controller_products.js
+++ assets/js/controller_products.js
@@ -5,5 +5,17 @@ angular.module('NabaRugsApp', [])
        .then(function(res){
           $scope.categories = res.data.products.categories;             
         });
-  });
+  })
+  .directive('onFinishRender', function ($timeout) {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attr) {
+            if (scope.$last === true) {
+                $timeout(function () {
+                    scope.$emit('ngRepeatFinished');
+                });
+            }
+        }
+    }
+});
 
