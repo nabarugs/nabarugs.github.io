85dd768d6153817aa3825490060d4272c1f286e5
ubu12 <ubu12@ubu12.(none)>
2016-05-12 17:16:59 -0400

    cat depends on url param

--- assets/js/controller_products.js
+++ assets/js/controller_products.js
@@ -5,8 +5,10 @@ angular.module('NabaRugsApp', [])
        .then(function(res){
           jsonObject = angular.fromJson(res.data);
           var cid = parseInt($location.search()['cid']);
+          console.log(cid);
           $scope.categories = jsonObject.products.categories;
-          $scope.visibleCat = $filter('filter')($scope.categories,{id:cid})[0];            
+          $scope.visibleCat = $filter('filter')($scope.categories,{id:cid})[0];
+          console.log($scope.visibleCat);            
         });
 
        $scope.$on('ngRepeatFinished', function(ngRepeatFinishedEvent) {
